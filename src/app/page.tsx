9044f5b7b55f28060a9a245d619f702b720e1dd3
Ali.r <alidev.r1996@gmail.com>
2025-03-31 02:39:47 +0430

    fix email address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
            </div>
 
            <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-rose-500 hover:text-white cursor-pointer">
-              <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-52 transition-all duration-300 text-sm">Mahdiyenaderi1377@gmail.com</p>
+              <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-52 transition-all duration-300 text-sm">Mahdiyehnaderi1377@gmail.com</p>
               <p className="size-16 flex items-center justify-center p-2 ">
                 <IoMailOutline className="size-full"/>
               </p>
