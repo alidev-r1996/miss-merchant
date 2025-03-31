2ef82f26bcda2c920794d4bf64622633cebc13d8
Ali.r <alidev.r1996@gmail.com>
2025-04-01 02:18:53 +0430

    fix icons-hover bug on mobile

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -12,33 +12,33 @@ const Contact = () => {
       </h1>
 
       <div
-        id="contact_us"
+        id="contact-us"
         className="flex flex-col md:flex-row items-center justify-center gap-1 md:mb-0"
       >
-        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white cursor-pointer">
+        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white focus:bg-green-500 focus:text-white cursor-pointer">
           <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 transition-all duration-300">
             00989028679502
           </p>
           <p className="size-16 flex items-center justify-center p-2">
-            <IoLogoWechat className="size-full" />
+            <IoLogoWechat className="size-16 flex items-center justify-center p-2" />
           </p>
         </div>
 
-        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-rose-500 hover:text-white cursor-pointer">
+        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-rose-500 hover:text-white active:bg-rose-500 active:text-white focus:bg-rose-500 focus:text-white cursor-pointer">
           <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-52 transition-all duration-300 text-sm">
             Mahdiyehnaderi1377@gmail.com
           </p>
           <p className="size-16 flex items-center justify-center p-2 ">
-            <IoMailOutline className="size-full" />
+            <IoMailOutline className="size-16 flex items-center justify-center p-2" />
           </p>
         </div>
 
-        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white cursor-pointer">
+        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white focus:bg-green-500 focus:text-white active:bg-green-500 active:text-white cursor-pointer">
           <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 transition-all duration-300">
             00989028679502
           </p>
           <p className="size-16 flex items-center justify-center p-2 ">
-            <IoLogoWhatsapp className="size-full" />
+            <IoLogoWhatsapp className="size-16 flex items-center justify-center p-2" />
           </p>
         </div>
       </div>
