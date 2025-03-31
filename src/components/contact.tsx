7ea24a83c5bb14d5b6503eccdfa6fcf8879766cb
Ali.r <alidev.r1996@gmail.com>
2025-04-01 02:23:36 +0430

    fix bug

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -16,7 +16,7 @@ const Contact = () => {
         className="flex flex-col md:flex-row items-center justify-center gap-1 md:mb-0"
       >
         <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white focus:bg-green-500 focus:text-white cursor-pointer">
-          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 transition-all duration-300">
+          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 group-active:w-32 group-focus:w-32 transition-all duration-300">
             00989028679502
           </p>
           <p className="size-16 flex items-center justify-center p-2">
@@ -25,7 +25,7 @@ const Contact = () => {
         </div>
 
         <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-rose-500 hover:text-white active:bg-rose-500 active:text-white focus:bg-rose-500 focus:text-white cursor-pointer">
-          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-52 transition-all duration-300 text-sm">
+          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-52 group-active:w-52 group-focus:w-52 transition-all duration-300 text-sm">
             Mahdiyehnaderi1377@gmail.com
           </p>
           <p className="size-16 flex items-center justify-center p-2 ">
@@ -34,7 +34,7 @@ const Contact = () => {
         </div>
 
         <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white focus:bg-green-500 focus:text-white active:bg-green-500 active:text-white cursor-pointer">
-          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 transition-all duration-300">
+          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 group-active:w-32 group-focus:w-32 transition-all duration-300">
             00989028679502
           </p>
           <p className="size-16 flex items-center justify-center p-2 ">
