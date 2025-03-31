e1804eaf319ef61b31816a2ab73a7dc01adb1ed2
Ali.r <alidev.r1996@gmail.com>
2025-04-01 02:42:53 +0430

    fix social bug

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import { useTranslations } from "next-intl";
+import { useState } from "react";
 import { IoLogoWechat, IoLogoWhatsapp, IoMailOutline } from "react-icons/io5";
 
 const Contact = () => {
     const t = useTranslations('Header')
+    const [social, setSocial] = useState({email: false, wechat: false, whatsapp: false})
 
 
   return (
@@ -15,8 +19,8 @@ const Contact = () => {
         id="contact-us"
         className="flex flex-col md:flex-row items-center justify-center gap-1 md:mb-0"
       >
-        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white focus:bg-green-500 focus:text-white cursor-pointer">
-          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 group-active:w-32 group-focus:w-32 transition-all duration-300">
+        <div onClick={() => setSocial(prev => ({ ...prev, wechat: !prev.wechat }))} className={`${social.wechat && "bg-green-500 text-white"} flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white focus:bg-green-500 focus:text-white cursor-pointer`}>
+          <p className={`${social.wechat ? "w-32" : "w-0"} font-bold w-0 whitespace-nowrap overflow-hidden  group-focus:w-32 transition-all duration-300`}>
             00989028679502
           </p>
           <p className="size-16 flex items-center justify-center p-2">
@@ -24,8 +28,8 @@ const Contact = () => {
           </p>
         </div>
 
-        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-rose-500 hover:text-white active:bg-rose-500 active:text-white focus:bg-rose-500 focus:text-white cursor-pointer">
-          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-52 group-active:w-52 group-focus:w-52 transition-all duration-300 text-sm">
+        <div onClick={() => setSocial(prev => ({ ...prev, email: !prev.email }))} className={`${social.email && "bg-rose-500 text-white"} flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-rose-500 hover:text-white active:bg-rose-500 active:text-white focus:bg-rose-500 focus:text-white cursor-pointer`}>
+          <p className={`${social.email ? "w-56" : "w-0"} font-bold w-0 whitespace-nowrap overflow-hidden transition-all duration-300 text-sm`}>
             Mahdiyehnaderi1377@gmail.com
           </p>
           <p className="size-16 flex items-center justify-center p-2 ">
@@ -33,8 +37,8 @@ const Contact = () => {
           </p>
         </div>
 
-        <div className="flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white focus:bg-green-500 focus:text-white active:bg-green-500 active:text-white cursor-pointer">
-          <p className="font-bold w-0 whitespace-nowrap overflow-hidden group-hover:w-32 group-active:w-32 group-focus:w-32 transition-all duration-300">
+        <div onClick={() => setSocial(prev => ({ ...prev, whatsapp: !prev.whatsapp }))} className={`${social.whatsapp && "bg-green-500 text-white"} flex items-center rounded shadow group px-4 whitespace-nowrap transition-all duration-300 hover:bg-green-500 hover:text-white focus:bg-green-500 focus:text-white active:bg-green-500 active:text-white cursor-pointer`}>
+          <p className={`${social.whatsapp ? "w-32" : "w-0"} font-bold w-0 whitespace-nowrap overflow-hidden  transition-all duration-300`}>
             00989028679502
           </p>
           <p className="size-16 flex items-center justify-center p-2 ">
