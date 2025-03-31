e6a4ad4b5069a33871cd73801475ccd3e95ad067
Ali.r <alidev.r1996@gmail.com>
2025-03-31 13:57:06 +0430

    port is fixed

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,6 +6,7 @@ import FAQSection from "@/components/faqsection";
 import ConsultantSection from "@/components/consultantSection";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
+import PortSection from "@/components/PortsSection";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Header />
         <Herosection />
         <Services />
+        <PortSection />
         <FAQSection />
         <ConsultantSection />
         <Contact />
